0c658000d3ed66f5d5496d08f838ea04c4a7ac70
Shafiyauzma <shafiyauzma8@gmail.com>
2026-06-04 18:54:44 +0530

    Attendance Page UI Completed

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
         setMobileOpen(false)
     },[pathname])
 
-    const role = "ADMIN" || "EMPLOYEE";
+    const role = "" || "EMPLOYEE";
 
     const navItem = [
         {name: "Dashboard", href:"/dashboard", icon: LayoutGridIcon},
